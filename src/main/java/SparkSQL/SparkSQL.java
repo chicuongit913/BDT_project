1f6081d1b0bfc5ae7ad57d8a0e719503e89b3703
vttrang <vtrang@outlook.com>
2020-04-15 02:44:55 -0500

    Update SparkSQL.java

--- src/main/java/SparkSQL/SparkSQL.java
+++ src/main/java/SparkSQL/SparkSQL.java
@@ -1,62 +1,74 @@
-package SparkSQL;
+package SparkStreaming;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog;
-
-import HBase.HBaseCovidTable;
-import SparkStreaming.StreamingJob;
-
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Properties;
 
-public class SparkSQL {
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
-    public static void main(String[] args) throws IOException {
-    	InputStream input = StreamingJob.class.getClassLoader().getResourceAsStream("config.properties");
-    	  
-    	if (input == null) {
+import HBase.HBaseCovidTable;
+
+public final class StreamingJob {
+
+    private static HBaseCovidTable covidTable;
+
+    /**
+     *
+     * @param args [0] is table name, [1] is topic name
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        InputStream input = StreamingJob.class.getClassLoader().getResourceAsStream("config.properties");
+
+        if (input == null) {
             System.out.println("Sorry, unable to find config.properties");
             return;
         }
-    	
-    	Properties prop = new Properties();
-    	//load a properties file from class path, inside static method
+
+        Properties prop = new Properties();
+        //load a properties file from class path, inside static method
         prop.load(input);
-        
-    	 String catalog = "{\n" +
-    	            "\t \"table\":{\"namespace\":\"default\", \"name\":\"" + HBaseCovidTable.TABLE_NAME + "\", \"tableCoder\":\"PrimitiveType\"},\n" +
-    	            "\t \"rowkey\":\"key\",\n" +
-    	            "\t \"columns\":{\n" +
-    	            "\t\t \"Rowkey\":{\"cf\":\"rowkey\", \"col\":\"key\", \"type\":\"string\"},\n" +
-    	            "\t\t \""+ HBaseCovidTable.CF_1_CONFIRMED + "\":{\"cf\":\""+ HBaseCovidTable.CF_1_CONFIRMED + "\", \"col\":\"" + args[0] + "\", \"type\":\"string\"},\n" +
-    	            "\t\t \""+ HBaseCovidTable.CF_2_DEATH + "\":{\"cf\":\""+ HBaseCovidTable.CF_2_DEATH + "\", \"col\":\"" + args[0] + "\", \"type\":\"string\"},\n" +
-    	            "\t\t \""+ HBaseCovidTable.CF_3_RECOVERED + "\":{\"cf\":\""+ HBaseCovidTable.CF_3_RECOVERED + "\", \"col\":\"" + args[0] + "\", \"type\":\"string\"},\n" +
-    	            "\t\t \""+ HBaseCovidTable.CF_4_ACTIVE + "\":{\"cf\":\""+ HBaseCovidTable.CF_4_ACTIVE + "\", \"col\":\"" + args[0] + "\", \"type\":\"string\"}\n" +
-    	            "\t}\n" +
-    	            "}";
-    	
-        Map<String, String> optionsMap = new HashMap<>();
 
-        String htc = HBaseTableCatalog.tableCatalog();
+        SparkConf sparkConf = new SparkConf()
+                .setAppName(prop.getProperty("spark.streaming.application.name"))
+                .setMaster(prop.getProperty("spark.streaming.master"));;
 
-        optionsMap.put(htc, catalog);
+        // Create the context with 2 seconds batch size
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
 
-        SparkSession spark = SparkSession
-                .builder()
-                .master(prop.getProperty("spark.sql.master"))
-                .appName(prop.getProperty("spark.sql.application.name"))
-                .getOrCreate();
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put(prop.getProperty("bootstrap.servers.key"), prop.getProperty("bootstrap.servers.value"));
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put(prop.getProperty("group.id.key"), prop.getProperty("group.id.value"));
+        kafkaParams.put(prop.getProperty("auto.offset.reset.key"), prop.getProperty("auto.offset.reset.value"));
+        kafkaParams.put(prop.getProperty("enable.auto.commit.key"), prop.getProperty("enable.auto.commit.value"));
 
-        Dataset<Row> dataset = spark.read().options(optionsMap).format("org.apache.spark.sql.execution.datasources.hbase").load();
+        Collection<String> topics = Arrays.asList(args[1]);
 
-        dataset.createOrReplaceTempView(HBaseCovidTable.TABLE_NAME);
-       
-        spark.sql("SELECT * FROM " + HBaseCovidTable.TABLE_NAME).show();
-        System.out.println("Schema: " + catalog);
+        final JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(
+                jssc,
+                LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
+        );
+
+        covidTable = new HBaseCovidTable(args[0]);
+        messages.foreachRDD(rdd -> rdd.foreach(message ->  {
+            covidTable.insertData(message.value());
+        }));
+
+        jssc.start();
+        jssc.awaitTermination();
     }
 }
